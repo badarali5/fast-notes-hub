f56f75449ccc26fe94789107c4a65712c1b3d93f
badarali5 <badaralinaqvi512@gmail.com>
2025-08-21 06:26:57 +0500

    adding apk

---
+++ fast-notes-hub-main/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fastnoteshub.app',
+  appName: 'fastnoteshub',
+  webDir: 'out'
+};
+
+export default config;
